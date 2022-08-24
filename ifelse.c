642f7039a1176b245e7a001d13daa89a8f05a18f
Krishi34S <sarmakrishi@gmail.com>
2022-08-24 10:25:52 +0530

    ifelse.c

---
+++ ifelse.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter your earning\n");
+    scanf("%d",&a);
+    if(a>=500)
+    printf("Congratulations, keep earning!");
+    else
+    printf("Go work hard");
+    return 0;
+}
