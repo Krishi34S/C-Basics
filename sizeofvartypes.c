aa176b3c7faa53c7e9189fd1b841d2c4b6c80da3
Krishi34S <sarmakrishi@gmail.com>
2022-08-25 01:40:40 +0530

    Program to check size of Variable types

---
+++ sizeofvartypes.c
@@ -0,0 +1,10 @@
+//Program to check sizes of each primitive datatypes
+#include<stdio.h>
+int main()
+{
+    printf("size of int in bytes: %d\n",sizeof(int));
+    printf("size of char in bytes: %d\n",sizeof(char));
+    printf("size of float in bytes: %d\n",sizeof(float));
+    printf("size of double in bytes: %d\n",sizeof(double));
+    return 0;
+}
